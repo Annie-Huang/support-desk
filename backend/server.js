81594a67620e522d91c41e33b85b9d1348f68621
Annie Huang <annie.hn@gmail.com>
2022-04-21 22:13:39 +1000

    Enable express to take request body in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// This two lines enable the express to handle req body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
   // res.send('Hello');
   res.status(200).json({ message: 'Welcome to the Support Desk API' });
