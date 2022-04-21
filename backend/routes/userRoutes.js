614b51f3ebb8a031136819864fc4cf4fb254bb71
Annie Huang <annie.hn@gmail.com>
2022-04-21 21:41:14 +1000

    Change userRoutes.js to call registerUser and loginUser in the userController.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const { registerUser, loginUser } = require('../controllers/userController.js');
 
-router.post('/', (req, res) => {
-  res.send('Register Route');
-});
+// router.post('/', (req, res) => {
+//   res.send('Register Route');
+// });
+router.post('/', registerUser);
 
-router.post('/login', (req, res) => {
-  res.send('Login Route');
-});
+// router.post('/login', (req, res) => {
+//   res.send('Login Route');
+// });
+router.post('/login', loginUser);
 
 module.exports = router;
