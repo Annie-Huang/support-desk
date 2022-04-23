5b52449b321df2c2ca47e3f75f04bf623612cb48
Annie Huang <annie.hn@gmail.com>
2022-04-23 22:03:32 +1000

    Create 3 cases under extraReducer for register.pending|fulfilled|rejected

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { fetchCount } from '../counter/counterAPI';
+import authService from './authService';
 
 const initialState = {
   user: null,
@@ -36,7 +37,23 @@ export const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(register.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(register.fullfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.user = action.payload;
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = true;
+        state.message = action.payload; // will pass in through thunkAPI.rejectWithValue(message);
+        state.user = null;
+      });
+  },
 });
 
 export default authSlice.reducer;
