ee704af421ac23079e99d4bcbdbc7c2a15b2c284
Annie Huang <annie.hn@gmail.com>
2022-04-23 18:55:03 +1000

    Swtich to React 18 syntax

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,23 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <Provider store={store}>
+    <App tab='home' />
+  </Provider>
 );
 
 // If you want your app to work offline and load faster, you can change
