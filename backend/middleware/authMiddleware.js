1f10bbe20fbb84cc58d053dbde57231a80d1b762
Annie Huang <annie.hn@gmail.com>
2022-04-23 12:21:49 +1000

    Create authMiddleware.js to do protect api call. Get token and abstract id token and then attach it back to req.user

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('express-async-handler');
+
+const User = require('../models/userModel');
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      // Get token from header
+      // req.headers.authorization = 'Bearer Token', so req.headers.authorization.split(' ') will return ['Bearer', 'Token']
+      console.log('req.headers.authorization=', req.headers.authorization);
+      token = req.headers.authorization.split(' ')[1];
+      console.log('token=', token);
+      // Verify token
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      // Get suer from token
+      req.user = await User.findById(decoded.id).select('-password');
+
+      next();
+    } catch (error) {}
+  }
+});
