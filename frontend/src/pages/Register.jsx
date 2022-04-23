8234d959baa1fc849815d998921f96bad6daab62
Annie Huang <annie.hn@gmail.com>
2022-04-23 22:50:59 +1000

    Add proxy to 5000 in package.json because our frontend is running in 3000 but backend is in 5000. Confirmed we can see 'User already exists' message

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { FaUser } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
-import { register } from '../features/auth/authSlice';
+import { register, reset } from '../features/auth/authSlice';
 
 const Register = () => {
   const [formData, setFormData] = useState({
