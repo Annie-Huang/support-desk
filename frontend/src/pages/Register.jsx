534f51c875629efaf0194a74c44e062fc3cef6fa
Annie Huang <annie.hn@gmail.com>
2022-04-23 16:46:56 +1000

    Create the whole form template in Register.jsx and the onChange function

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -11,6 +11,13 @@ const Register = () => {
 
   const { name, email, password, password2 } = formData;
 
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
   return (
     <>
       <section className='heading'>
@@ -18,6 +25,58 @@ const Register = () => {
           <FaUser /> Register
         </h1>
         <p>Please create an account</p>
+      </section>
+
+      <section className='form'>
+        <form>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              id='name'
+              name='name'
+              value={name}
+              onChange={onChange}
+              placeholder='Enter your name'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              id='email'
+              name='email'
+              value={email}
+              onChange={onChange}
+              placeholder='Enter your email'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password'
+              name='password'
+              value={password}
+              onChange={onChange}
+              placeholder='Enter password'
+            />
+          </div>
+          <div className='form-group'>
+            <input
+              type='password'
+              className='form-control'
+              id='password2'
+              name='password2'
+              value={password2}
+              onChange={onChange}
+              placeholder='Confirm password'
+            />
+          </div>
+          <div className='form-group'>
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
       </section>
     </>
   );
