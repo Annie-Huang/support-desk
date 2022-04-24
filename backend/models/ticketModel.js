05027596f4ab2463c39d82c2e3025044de9e0759
Annie Huang <annie.hn@gmail.com>
2022-04-24 14:22:04 +1000

    Add user, product, description, status field for the ticketModel.js

--- backend/models/ticketModel.js
+++ backend/models/ticketModel.js
@@ -2,23 +2,25 @@ const mongoose = require('mongoose');
 
 const ticketSchema = mongoose.Schema(
   {
-    name: {
-      type: String,
-      required: [true, 'Please add a name'],
-    },
-    email: {
-      type: String,
-      required: [true, 'Please add an email'],
-      unique: true,
-    },
-    password: {
-      type: String,
-      required: [true, 'Please add a password'],
-    },
-    isAdmin: {
-      type: Boolean,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
       required: true,
-      default: false,
+      ref: 'User',
+    },
+    product: {
+      type: String,
+      required: [true, 'Please select a product'],
+      enum: ['iPhone', 'Macbook Pro', 'iMac', 'iPad'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please enter a description of the issue'],
+    },
+    status: {
+      type: String,
+      required: true,
+      enum: ['new', 'open', 'closed'],
+      default: 'new',
     },
   },
   {
