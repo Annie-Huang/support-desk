39c062c2560e8b796580cb9e8c6baa9a08787d17
Annie Huang <annie.hn@gmail.com>
2022-04-24 16:41:52 +1000

    Create two functions in the ticketController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 
 // @desc    Register a new user
-// @route   /api/users
+// @route   POST /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   // console.log(req.body);
@@ -54,7 +54,7 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 // @desc    Login a user
-// @route   /api/users/login
+// @route   POST /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
@@ -76,7 +76,7 @@ const loginUser = asyncHandler(async (req, res) => {
 });
 
 // @desc    Get current user
-// @route   /api/users/me
+// @route   GET /api/users/me
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
   // res.send('me');
