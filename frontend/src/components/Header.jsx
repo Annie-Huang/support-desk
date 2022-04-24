373c9370185174538e90d635dc7b6112b1072ec8
Annie Huang <annie.hn@gmail.com>
2022-04-24 12:31:06 +1000

    Add all the import for useNavigate, useDispatch, useSelector in Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,7 +1,14 @@
 import { FaSignInAlt, FaSignOutAlt, FaUser } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../features/auth/authSlice';
 
 const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <header className='header'>
       <div className='logo'>
