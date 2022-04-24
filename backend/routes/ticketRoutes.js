5a44a7c8fcbf747d10f459e45935788d19a928a9
Annie Huang <annie.hn@gmail.com>
2022-04-24 16:52:57 +1000

    Import controller function into ticketRoutes.js. Add reference to ticketRoutes in server.js

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const { getTickets, createTicket } = require('../controllers/ticketController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 // If you don't want to chain this, you can follow the example in the userRoutes.js
-router.route('/').get(protect, getTicket).post(protect, createTicket);
+router.route('/').get(protect, getTickets).post(protect, createTicket);
 
 module.exports = router;
