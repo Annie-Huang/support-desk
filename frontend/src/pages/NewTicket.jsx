a2bfd62e281f268367af866a1a477030ac69933f
Annie Huang <annie.hn@gmail.com>
2022-04-24 21:32:58 +1000

    Create all local state for NewTicket.jsx

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -1,4 +1,13 @@
+import { useSelector } from 'react-redux';
+import { useState } from 'react';
+
 const NewTicket = () => {
+  const { user } = useSelector((state) => state.auth);
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
+  const [product, setProduct] = useState('iPhone');
+  const [description, setDescription] = useState('');
+
   return (
     <div>
       <h1>New Ticket</h1>
