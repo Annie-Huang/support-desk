551355a90d45abcc122dd8691731d4456c71249c
Annie Huang <annie.hn@gmail.com>
2022-04-24 22:02:41 +1000

    Create the disbled name and email input field in NewTicket.jsx template

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -14,6 +14,30 @@ const NewTicket = () => {
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
       </section>
+
+      <section className='form'>
+        <div className='form-group'>
+          {/* label htmlFor need to match input id */}
+          <label htmlFor='name'>Customer Name</label>
+          <input
+            type='text'
+            className='form-control'
+            id='name'
+            value={name}
+            disabled
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Customer Email</label>
+          <input
+            type='text'
+            className='form-control'
+            id='email'
+            value={email}
+            disabled
+          />
+        </div>
+      </section>
     </>
   );
 };
