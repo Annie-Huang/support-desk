fab3f3342b89afb8847c2526389101b0960ea125
Annie Huang <annie.hn@gmail.com>
2022-04-24 21:58:44 +1000

    We don't need to the setName and setEmail method.

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 
 const NewTicket = () => {
   const { user } = useSelector((state) => state.auth);
-  const [name, setName] = useState(user.name);
-  const [email, setEmail] = useState(user.email);
+  const [name] = useState(user.name);
+  const [email] = useState(user.email);
   const [product, setProduct] = useState('iPhone');
   const [description, setDescription] = useState('');
 
