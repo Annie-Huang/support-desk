d44571ef2e0f09a7d8e8f67308e0904893befb60
Annie Huang <annie.hn@gmail.com>
2022-04-24 16:11:00 +1000

    Add route for get and post in ticketRoutes.js

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 const { protect } = require('../middleware/authMiddleware');
 
+// If you don't want to chain this, you can follow the example in the userRoutes.js
+router.route('/').get(protect, getTicket).post(protect, createTicket);
+
 module.exports = router;
