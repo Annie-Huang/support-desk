a5832edabfad43ac89d0feaef261282810517647
Annie Huang <annie.hn@gmail.com>
2022-04-24 22:26:45 +1000

    Create boiletplate for createSlice on ticketSlice and export the action and reducer.

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -9,3 +9,16 @@ const initialState = {
   isLoading: false,
   message: '',
 };
+
+export const ticketSlice = createSlice({
+  name: 'ticket',
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { reset } = ticketSlice.actions;
+
+export default ticketSlice.reducer;
