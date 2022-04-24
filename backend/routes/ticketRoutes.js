43eca6c7ab648bdcd46c88521ef27fe4914c5a3b
Annie Huang <annie.hn@gmail.com>
2022-04-24 18:39:00 +1000

    Create a route in ticketRoutes.js to call getTicket

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { getTickets, createTicket } = require('../controllers/ticketController');
+const {
+  getTickets,
+  getTicket,
+  createTicket,
+} = require('../controllers/ticketController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 // If you don't want to chain this, you can follow the example in the userRoutes.js
 router.route('/').get(protect, getTickets).post(protect, createTicket);
+router.route('/:id').get(protect, getTicket);
 
 module.exports = router;
