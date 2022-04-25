29ba298eb75befa2441a3d5aa482ca7bee0a6f55
Annie Huang <annie.hn@gmail.com>
2022-04-25 14:54:31 +1000

    Create getTicket in ticketService.js

--- frontend/src/features/tickets/ticketService.js
+++ frontend/src/features/tickets/ticketService.js
@@ -29,6 +29,23 @@ const getTickets = async (token) => {
   return response.data;
 };
 
-const ticketService = { createTicket, getTickets };
+// Get user ticket
+const getTicket = async (ticketId, token) => {
+  const config = {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  // C:\react\support-desk\frontend\node_modules\axios\index.d.ts
+  // Very annoying Brad didn't make this consistent on ticket vs tickets:
+  // for frontend url, it got http://localhost:3000/ticket/6265f3b59614e3e80dd035b7
+  // for backend api, it got  http://localhost:5000/api/tickets/6265f3b59614e3e80dd035b7
+  const response = await axios.get(`${API_URL}/${ticketId}`, config);
+
+  return response.data;
+};
+
+const ticketService = { createTicket, getTickets, getTicket };
 
 export default ticketService;
