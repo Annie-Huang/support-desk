258299ef9264da6adb994559e3d2f83ecde0ffd5
Annie Huang <annie.hn@gmail.com>
2022-04-25 18:36:21 +1000

    Create closeTicket in ticketSlice.js, which is similar to getTicket

--- frontend/src/features/tickets/ticketSlice.js
+++ frontend/src/features/tickets/ticketSlice.js
@@ -64,6 +64,24 @@ export const getTicket = createAsyncThunk(
   }
 );
 
+// Close ticket
+export const closeTicket = createAsyncThunk(
+  'tickets/close',
+  async (ticketId, thunkAPI) => {
+    try {
+      // thunkAPI has access to the store state
+      const token = thunkAPI.getState().auth.user.token;
+
+      return await ticketService.closeTicket(ticketId, token);
+    } catch (error) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const ticketSlice = createSlice({
   name: 'ticket',
   initialState,
