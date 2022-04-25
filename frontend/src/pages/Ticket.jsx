fedcc711630a7a6a90910d012d22d3df56219ad0
Annie Huang <annie.hn@gmail.com>
2022-04-25 14:59:26 +1000

    Add all the import and useSelector and useParams in Ticket.jsx

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,4 +1,16 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { getTicket, reset } from '../features/tickets/ticketSlice';
+import Spinner from '../components/Spinner';
+import BackButton from '../components/BackButton';
+import { useParams } from 'react-router-dom';
+
 const Ticket = () => {
+  const { ticket, isLoading, isSuccess } = useSelector(
+    (state) => state.tickets
+  );
+
+  const params = useParams();
+
   return <div>Ticket</div>;
 };
 
