5447c1419131e70b8928886fb31c135884529693
Annie Huang <annie.hn@gmail.com>
2022-04-25 15:07:17 +1000

    On page load (e.g. ticketId changes), make a call to fetch ticket, and if there is error in return, toast message

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -3,13 +3,24 @@ import { getTicket, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../components/Spinner';
 import BackButton from '../components/BackButton';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 
 const Ticket = () => {
-  const { ticket, isLoading, isSuccess } = useSelector(
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
     (state) => state.tickets
   );
 
-  const params = useParams();
+  const { ticketId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    dispatch(getTicket(ticketId));
+  }, [isError, message, ticketId]);
 
   return <div>Ticket</div>;
 };
