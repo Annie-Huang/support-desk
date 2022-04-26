f6a551a794c1fa36c54f716ae7051718003488f5
Annie Huang <annie.hn@gmail.com>
2022-04-26 21:37:08 +1000

    Change to getNotes, besides validating the user, also get single ticket and validate the ticket against the user.

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const Ticket = require('../models/ticketModel');
+const Note = require('../models/noteModel');
 
-// @desc    Get user tickets
-// @route   GET /api/tickets
+// @desc    Get notes for a ticket
+// @route   GET /api/tickets/:ticketId/notes
 // @access  Private
-const getTickets = asyncHandler(async (req, res) => {
+const getNotes = asyncHandler(async (req, res) => {
   // Get user using the id in the JWT
   const user = await User.findById(req.user.id);
 
@@ -14,8 +15,14 @@ const getTickets = asyncHandler(async (req, res) => {
     throw new Error('User not found');
   }
 
-  const tickets = await Ticket.find({ user: req.user.id });
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  // Not sure why we are not checking !ticket like we did for getTicket under ticketController.js
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
 
   res.status(200).json(tickets);
-  // res.status(200).json({ message: 'getTickets' });
 });
