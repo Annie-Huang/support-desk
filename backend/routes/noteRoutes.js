b0b565755a4b6d433be3e8b64b12536400125589
Annie Huang <annie.hn@gmail.com>
2022-04-27 18:00:06 +1000

    Add 'addNote' to noteRoutes.js

--- backend/routes/noteRoutes.js
+++ backend/routes/noteRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true }); // this part is different from other routes
-const { getNotes } = require('../controllers/noteController');
+const { getNotes, addNote } = require('../controllers/noteController');
 
 const { protect } = require('../middleware/authMiddleware');
 
 // The route we want to get is: /api/tickets/:ticketId/notes
 // The router is used in C:\react\support-desk\backend\routes\ticketRoutes.js
-router.route('/').get(protect, getNotes);
+router.route('/').get(protect, getNotes).post(protect, addNote);
 
 module.exports = router;
