019624cb01c27ba168694c1349ec08d2a976363f
Annie Huang <annie.hn@gmail.com>
2022-04-27 21:49:19 +1000

    Create boilerplate for noteSlice and export the actions and reducer

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -8,3 +8,16 @@ const initialState = {
   isLoading: false,
   message: '',
 };
+
+export const noteSlice = createSlice({
+  name: 'note',
+  initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {},
+});
+
+export const { reset } = noteSlice.actions;
+
+export default noteSlice.reducer;
