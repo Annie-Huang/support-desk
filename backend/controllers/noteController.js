dc4699c38458529492de50cb037c1b65b40818d9
Annie Huang <annie.hn@gmail.com>
2022-04-27 17:53:45 +1000

    Copy getNotes to addNote in noteController.js

--- backend/controllers/noteController.js
+++ backend/controllers/noteController.js
@@ -44,6 +44,32 @@ const getNotes = asyncHandler(async (req, res) => {
   res.status(200).json(notes);
 });
 
+// @desc    Get notes for a ticket
+// @route   GET /api/tickets/:ticketId/notes
+// @access  Private
+const addNote = asyncHandler(async (req, res) => {
+  // Get user using the id in the JWT
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  // Not sure why we are not checking !ticket like we did for getTicket under ticketController.js
+
+  if (ticket.user.toString() !== req.user.id) {
+    res.status(401);
+    throw new Error('User not authorized');
+  }
+
+  const notes = await Note.find({ ticket: req.params.ticketId });
+
+  res.status(200).json(notes);
+});
+
 module.exports = {
   getNotes,
 };
