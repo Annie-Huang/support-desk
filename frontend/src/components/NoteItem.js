8b7fd65676227fc8cf455c94fef7bdf5e686f7f3
Annie Huang <annie.hn@gmail.com>
2022-04-27 22:26:12 +1000

    Create boilerplate of NoteItem.js and get user from user slice.

---
+++ frontend/src/components/NoteItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+const NoteItem = ({ note }) => {
+  const { user } = useSelector((state) => state.auth);
+
+  return <div></div>;
+};
+
+export default NoteItem;
