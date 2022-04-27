fdfddfb0082c1ff4131d4ae39419c15b1cbf5aa9
Annie Huang <annie.hn@gmail.com>
2022-04-27 21:54:59 +1000

    Copy getTicket from ticketSlice to getNotes in noteSlice.js

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import noteService from './noteService';
+import ticketService from '../tickets/ticketService';
 
 const initialState = {
   notes: [],
@@ -8,6 +9,24 @@ const initialState = {
   isLoading: false,
   message: '',
 };
+
+// Get user ticket
+export const getNotes = createAsyncThunk(
+  'tickets/get',
+  async (ticketId, thunkAPI) => {
+    try {
+      // thunkAPI has access to the store state
+      const token = thunkAPI.getState().auth.user.token;
+
+      return await ticketService.getTicket(ticketId, token);
+    } catch (error) {
+      const message =
+        error?.response?.data?.message || error.message || error.toString();
+
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const noteSlice = createSlice({
   name: 'note',
