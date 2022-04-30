efec7e37bd7fcf187479638472a4db96646b28b6
Annie Huang <annie.hn@gmail.com>
2022-04-30 14:46:34 +1000

    Update the logic of createNote in noteService.js

--- frontend/src/features/notes/noteService.js
+++ frontend/src/features/notes/noteService.js
@@ -15,19 +15,23 @@ const getNotes = async (ticketId, token) => {
   return response.data;
 };
 
-// Get ticket notes
-const createNote = async (ticketId, token) => {
+// Create ticket note
+const createNote = async (noteText, ticketId, token) => {
   const config = {
     headers: {
       Authorization: `Bearer ${token}`,
     },
   };
 
-  const response = await axios.get(`${API_URL}/${ticketId}/notes`, config);
+  const response = await axios.post(
+    `${API_URL}/${ticketId}/notes`,
+    { text: noteText },
+    config
+  );
 
   return response.data;
 };
 
-const noteService = { getNotes };
+const noteService = { getNotes, createNote };
 
 export default noteService;
