91f588c13c4c7786d75f41259aec4e1f85ec4bde
Annie Huang <annie.hn@gmail.com>
2022-04-30 14:30:53 +1000

    Update the createNote asyncThunk function

--- frontend/src/features/notes/noteSlice.js
+++ frontend/src/features/notes/noteSlice.js
@@ -27,15 +27,15 @@ export const getNotes = createAsyncThunk(
   }
 );
 
-// Get ticket notes
-export const CreateNote = createAsyncThunk(
-  'notes/getAll',
-  async (ticketId, thunkAPI) => {
+// Create ticket note
+export const createNote = createAsyncThunk(
+  'notes/create',
+  async ({ noteText, ticketId }, thunkAPI) => {
     try {
       // thunkAPI has access to the store state
       const token = thunkAPI.getState().auth.user.token;
 
-      return await noteService.getNotes(ticketId, token);
+      return await noteService.createNote(noteText, ticketId, token);
     } catch (error) {
       const message =
         error?.response?.data?.message || error.message || error.toString();
